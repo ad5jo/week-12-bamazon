3b692a6af4e27290010cf5fa81ccb5220505f936
Dave Durkee <ad5jo@yahoo.com>
2017-02-02 08:24:25 -0600

    almost finished - have bug in the update code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+const prompt = require("prompt");
 var inquirer = require("inquirer");
 
 
@@ -23,19 +24,19 @@ function inquireMain() {
     inquirer.prompt([
         {
             type: "list",
-            message: "Welcome to Flashcard App, Select an Option Below:",
-            choices: ["POST AN ITEM", "BID ON AN ITEM", "QUIT"],
+            message: "Welcome to BAMazon:",
+            choices: ["List items","Purchase", "QUIT"],
             name: "command"
         }
     ]).then(function (user) {
         switch (user.command) {
-            case "POST AN ITEM":
-                console.log('POST');
-                inquireItem();
+            case "List items":
+                console.log(' -list items- ');
+                inquireList();
                 break;
-            case "BID ON AN ITEM":
-                console.log('BID');
-                inquireBid();
+            case "Purchase":
+                console.log(' -Purchase- ');
+                inquire_purchase();
                 break;
             case "QUIT":
                 console.log("Goodbye!");
@@ -44,47 +45,127 @@ function inquireMain() {
     });
 }
 
-function inquireItem() {
+// function inquireItem() {
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             message: "Enter your item for bid: ",
+//             name: "item"
+//         },
+//         {
+//             type: "input",
+//             message: "Enter your item's starting bid: ",
+//             name: "bid"
+//         }
+//     ]).then(function (user) {
+
+//         connection.query("INSERT INTO item SET ?", {
+//             item_name: user.item,
+//             start_price: user.bid
+//         }, function (err, res) { });
+//         inquireMain();
+//     });
+// }
+
+
+// UPDATE Customers
+// SET City = 'Hamburg'
+// WHERE CustomerID = 1;
+
+// function inquire_purchase() {
+//     connection.query("UPDATE products SET quantity = ",
+//         function (err, res) {
+//             for (var i = 0; i < res.length; i++) {
+//                 //console.log(res[i].item_name);
+//                 items.push(res[i].item_name);
+
+//             }
+//             inquirer.prompt([
+//                 {
+//                     type: "list",
+//                     message: "Select an item to bid",
+//                     choices: items,
+//                     name: "selected"
+//                 },
+//             ]).then(function (user) {
+//                 console.log(user.selected);
+//             });
+//         });
+// }
+
+function inquire_purchase() {
     inquirer.prompt([
         {
             type: "input",
-            message: "Enter your item for bid: ",
-            name: "item"
+            message: "item number",
+            name: "id"
         },
+        ])
+        .then(function (user) {
+            console.log(user.id);
+            }
+        );
+    inquirer.prompt([
         {
             type: "input",
-            message: "Enter your item's starting bid: ",
-            name: "bid"
-        }
-    ]).then(function (user) {
+            message: "How many",
+            name: "qty"
+        },
+        ])
+        .then(function (user) {
+            console.log(user.qty);
+            }
+        );
 
-        connection.query("INSERT INTO item SET ?", {
-            item_name: user.item,
-            start_price: user.bid
-        }, function (err, res) { });
-        inquireMain();
-    });
+    var s_temp = "SELECT stock_quantity FROM products WHERE item_id = " + user.id;
+    var s_count_query = '\"s_temp\"';
+    connection.query(s_count_query,function (err, res) {
+            // do somethin
+            var i_temp_qty = res.stock_quantity;
+            if (res.stock_quantity >= user.qty)
+            {
+                i_temp_qty = i_temp_qty - user.qty;
+            }
+            else
+            {
+                console.log("-----------------------------------------");
+                console.log("Sorry, Can't complete you order. Not enough inventory. Try ordering fewer.");
+                console.log("-----------------------------------------");
+                inquireMain();
+            }
+        })
+        .then ( // then A
+            //var 
+            s_temp = "UPDATE products SET stock_quantity = " + i_temp_qty;
+            s_temp = s_temp + " WHERE item_id = " + user.id;
+            var s_ud_query = '\"s_temp\"';
+            connection.query(s_ud_query,function (err, res) {
+                // do somethin
+                console.log("success");
+                inquireMain();
+                console.log("-----------------------------------------");
+                console.log("\n");
+            });
+        ); // then A
+
 }
 
-function inquireBid() {
-    var items = [];
-    connection.query("SELECT * FROM item",
-        function (err, res) {
-            for (var i = 0; i < res.length; i++) {
-                //console.log(res[i].item_name);
-                items.push(res[i].item_name);
 
+function inquireList() {
+    var items = [];
+    connection.query("SELECT * FROM products",
+        function (err, res) {
+            console.log("\n-----------------------------------------");
+            console.log(" -inventory- ");
+            console.log("");
+            for (var i = 0; i < res.length; i++) {
+                console.log(res[i].item_id + "\t" + res[i].product_name);
+                //console.log("\t" );
+                //console.log(res[i].product_name );
+                items.push(res[i].product_name);
             }
-            inquirer.prompt([
-                {
-                    type: "list",
-                    message: "Select an item to bid",
-                    choices: items,
-                    name: "selected"
-                },
-            ]).then(function (user) {
-                console.log(user.selected);
-            });
+            console.log("-----------------------------------------");
+            console.log("\n");
         });
 
 }
